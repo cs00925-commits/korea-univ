2eefdcde1f5b9ded6abd2f99e63b94ac03298602
cs00925-commits <cs00925@gmail.com>
2026-04-21 05:41:13 +0000

    자기소개 섹션 추가 및 텍스트 입력 기능 구현

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
 st.title("🎈 My new app")
+
+st.header("자기소개")
+
+introduction = st.text_area("자기소개를 입력하세요:", "안녕하세요. 특강 재미있어요.")
+
+if introduction:
+    st.write("**당신의 자기소개:**")
+    st.write(introduction)
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
